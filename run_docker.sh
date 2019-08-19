c02367482b2ebd2b98cbe59164016596251ad7e7
Austin Zheng <zhengbill@live.cn>
2019-08-19 11:29:11 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,8 +4,8 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t html-website:v1 .
+sudo docker build -t html-website:v1 .
 
 # Step 2: 
 # Run the html page
-docker run -d -p 80:80 html-website:v1
+sudo docker run -d -p 80:80 html-website:v1
